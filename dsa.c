22bc8996cdc3518889a23a01828379110f4d6f33
Oskar Thoren <o.thoren@gmail.com>
2013-06-14 22:16:52 +0200

    array list with dynamic resize

--- dsa.c
+++ dsa.c
@@ -2,44 +2,62 @@
 //
 // lists - array list, linked list, stack, queue; mappings; trees; graph
 // traversal - BFS, DFS; and more.
+//
+// no error handling here
 
+#include <stdlib.h>
 #include <stdio.h>
 
-#define MAX 5
-
 // array list
+// from casablanca at stackoverflow
+// todo: insert at pos; print; delete; locate.
 
-/* struct arraylist { */
-/*   int list[MAX]; */
-/*   int last; // where init this? */
-/* }; */
+typedef struct {
+  int *array;
+  size_t used, size;
+} Array;
 
-int saveto(int lst[]) {
-  int i, c;
-
-  while ((c = getchar()) != EOF)
-    for (i = 0; i < MAX; i++) // don't know end, this is a potential problem.
-      scanf("%d", &lst[i]); // save the scanned digit to the _address_
-
-  return 0;
+void initArray(Array *a, size_t initSize) {
+  a->array = (int *)malloc(initSize * sizeof(int));
+  a->used = 0;
+  a->size = initSize;
 }
 
-int write(int lst[]) {
-  int i;
-
-  for (i = 0; i < MAX; i++)
-    printf("%d\n", lst[i]);
-
-  return 0;
+// for insert at position, shift by one
+void insertArray(Array *a, int element) {
+  if (a->used == a->size) {
+    a->size *= 2; /* dynamic resize */
+    a->array = (int *)realloc(a->array, a->size * sizeof(int));
+  }
+  a->array[a->used++] = element;
 }
 
-int main() {
-  int lst[MAX];
-
-  saveto(lst);
-  write(lst);
-
-  return 0;
+void freeArray(Array *a) {
+  free(a->array);
+  a->array = NULL;
+  a->used = a->size = 0;
 }
 
 // linked list
+
+
+
+
+
+
+
+
+
+
+// main function
+int main() {
+
+  // array list play
+  Array a;
+  initArray(&a, 5);
+  for (int i = 0; i < 100; i++)
+    insertArray(&a, i);
+  printf("%d\n", a.array[9]);
+  printf("%d\n", (int)a.used);
+  freeArray(&a);
+}
